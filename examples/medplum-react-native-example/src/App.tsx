dd461c238aa3154a896f20168aa308af11299efc
Derrick Farris <mr.dcfarris@gmail.com>
2023-09-02 16:59:06 -0500

    fix(react-native-example): add polyfills so `MedplumClient` works on RN

--- examples/medplum-react-native-example/src/App.tsx
+++ examples/medplum-react-native-example/src/App.tsx
@@ -1,4 +1,5 @@
 import { getDisplayString, LoginAuthenticationResponse, MedplumClient, ProfileResource } from '@medplum/core';
+import { polyfillMedplumWebAPIs } from '@medplum/expo-medplum-polyfills';
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
@@ -6,10 +7,21 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 const medplum = new MedplumClient({
   // Enter your Medplum connection details here
   // See MedplumClient docs for more details
-  // baseUrl: "http://localhost:8103/",
+  baseUrl: 'http://localhost:8103',
+  // ------------------------------------------------------------------------------
+  // If you are testing this out with your physical Android / iOS device and not an emulator,
+  // you will need to put your computer's local IP address here, for example:
+  // baseUrl: 'http://192.168.x.x:8103'
+  // Metro will usually emit this address in the line: 'Metro waiting on exp://192.168.1.216:8081'
+  // but you will need to change the protocol to 'http://' and the port to 8103 (the Medplum server's default) or whatever port your server is using
+  // ------------------------------------------------------------------------------
   // clientId: 'MY_CLIENT_ID',
   // projectId: 'MY_PROJECT_ID',
 });
+
+// This is a module to get the Medplum client working on React Native by polyfilling a few Web APIs that are missing from the React Native runtime
+// On web, this is a no-op
+polyfillMedplumWebAPIs(medplum);
 
 export default function App(): JSX.Element {
   const [email, setEmail] = useState('');
@@ -45,6 +57,7 @@ export default function App(): JSX.Element {
   function signOut(): void {
     setProfile(undefined);
     medplum.signOut().catch(console.error);
+    sessionStorage.clear();
   }
 
   return (
@@ -85,15 +98,16 @@ export default function App(): JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    gap: 10,
+    backgroundColor: '#ecf0f1',
   },
   input: {
-    height: 50,
-    flex: 1,
+    width: 200,
+    height: 44,
     padding: 10,
-    marginLeft: 20,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
   },
 });
